4c33473e6d85ebf2d2b555bf6120a553b4ec02e0
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2020-10-08 21:54:51 +0200

    ajout de router et premier lien /react avec donnée trier (mode sale)

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,5 +1,7 @@
 // == Import npm
 import React from 'react';
+// Composant route : faire un rendu en fonction de l'url courante.
+import { Route } from 'react-router-dom';
 
 // == Import
 import categoriesData from 'src/data/categories';
@@ -10,13 +12,22 @@ import Posts from '../Posts';
 import './blog.scss';
 
 // == Composant
-const Blog = () => (
-  <div className="blog">
-    <Header categories={categoriesData} />
-    <Posts posts={postsData} />
-    <Footer />
-  </div>
-);
+const Blog = () => {
+  const postsReact = postsData.filter((post) => post.category === 'React');
+
+  return (
+    <div className="blog">
+      <Header categories={categoriesData} />
+      <Route exact path="/">
+        <Posts posts={postsData} />
+      </Route>
+      <Route path="/react">
+        <Posts posts={postsReact} />
+      </Route>
+      <Footer />
+    </div>
+  );
+};
 
 // == Export
 export default Blog;
