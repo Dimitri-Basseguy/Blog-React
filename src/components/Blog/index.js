536e35f7200d84e1957db81ae2dd53b020d9a66f
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2020-10-18 17:53:43 +0200

    styling

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,9 +1,14 @@
 // == Import npm
-import React from 'react';
+import React, { useState } from 'react';
 // Composant route : faire un rendu en fonction de l'url courante. 
 // + Composant switch : seulement la premier route qui correspond est rendue 
 // => utile pour page d'erreur 404
-import { Link, Route, Switch, Redirect } from 'react-router-dom';
+import {
+  Link,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 
 // == Import
 import categoriesData from 'src/data/categories';
@@ -23,26 +28,37 @@ import './blog.scss';
 */
 
 // == Composant
-const Blog = () => (
-  <div className="blog">
-    <Header categories={categoriesData} />
-    <Switch>
-      <Redirect from="/jquery" to="/autre" />
-      {categoriesData.map((category) => (
-        <Route exact path={category.route} key={category.label}>
-          <Posts posts={getPostByCategory(postsData, category.label)} />
+const Blog = () => {
+  const [posts, setPosts] = useState([]);
+  const [categories, setCategories] = useState(categoriesData);
+  // console.log(categories);
+  // const [loading, setLoading] = useState(false);
+  const loadPost = () => {
+    setPosts(postsData);
+  };
+
+  return (
+    <div className="blog">
+      <Header categories={categories} />
+      <button type="button" onClick={loadPost}>Charger les articles</button>
+      <Switch>
+        <Redirect from="/jquery" to="/autre" />
+        {categories.map((category) => (
+          <Route exact path={category.route} key={category.label}>
+            <Posts posts={getPostByCategory(posts, category.label)} />
+          </Route>
+        ))}
+        <Route>
+          <div>
+            <img src="https://cdn.dribbble.com/users/1197927/screenshots/8062981/media/498b9f796d031f3c2370130510d63f2f.gif" alt="error 404" />
+            <h1>Sacrebleu ! Page non trouvée. (erreur 404) | <Link to="/">Retourner sur l'accueil</Link></h1>
+          </div>
         </Route>
-      ))}
-      <Route>
-        <div>
-          <img src="https://cdn.dribbble.com/users/1197927/screenshots/8062981/media/498b9f796d031f3c2370130510d63f2f.gif" alt="error 404"/>
-          <h1>Sacrebleu ! Page non trouvée. (erreur 404) | <Link to="/">Retourner sur l'accueil</Link></h1>
-        </div>
-      </Route>
-    </Switch>
-    <Footer />
-  </div>
-);
+      </Switch>
+      <Footer />
+    </div>
+  );
+};
 
 // == Export
 export default Blog;
