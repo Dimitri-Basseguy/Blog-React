b337bb62d5944ce320f1a0a3cd839b30415200dd
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2020-10-26 22:37:42 +0100

    ajout du DOMPurify pour nettoyer le HTML pas sur...

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import DOMPurify from 'dompurify';
 
 import './posts.scss';
+
+const createMarkup = (htmlCode) => {
+  return {
+    __html: DOMPurify.sanitize(htmlCode),
+  };
+};
 
 const Post = ({ title, category, excerpt }) => (
   <article className="post">
     <h2 className="post__title">{title}</h2>
     <div className="post__category">{category}</div>
-    <p className="post__excerpt">{excerpt}</p>
+    <p className="post__excerpt" dangerouslySetInnerHTML={createMarkup(excerpt)} />
   </article>
 );
 
