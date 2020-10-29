7cb4d2db7a46f07e8595a0de4dc53c7c4edacdf9
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2020-10-29 21:48:34 +0100

    ajout de commentaire type "docs"

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -13,7 +13,7 @@ import {
 import axios from 'axios';
 
 // == Import
-import categoriesData from 'src/data/categories';
+// import categoriesData from 'src/data/categories';
 // import postsData from 'src/data/posts';
 
 import { getPostByCategory } from 'src/utils/selectors';
@@ -35,11 +35,12 @@ import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 // == Composant
 const Blog = () => {
   const [posts, setPosts] = useState([]);
-  const [categories, setCategories] = useState(categoriesData);
+  const [categories, setCategories] = useState([]);
   // console.log(categories);
 
   /**  indique si on est en cours de chargement */
   const [loading, setLoading] = useState(true);
+  const [loadingCategories, setLoadingCategories] = useState(true);
 
   const loadPosts = () => {
     // affiche 'false' => la mise à jour du state est asynchrone (pareil avec
@@ -52,7 +53,7 @@ const Blog = () => {
     axios.get('https://oclock-open-apis.now.sh/api/blog/posts')
       .then((response) => {
         // callback exécutée en cas de succès (par exemple code de retour 200)
-        // console.log('success: ', response);
+        // console.log('success: ', response.data);
         setPosts(response.data);
       })
       .catch((error) => {
@@ -64,6 +65,33 @@ const Blog = () => {
         // permet notamment d'enlever un loader
         // console.log('finally');
         setLoading(false);
+      });
+    // console.log('on a lancé le chargement des articles');
+  };
+
+  const loadCategories = () => {
+    // affiche 'false' => la mise à jour du state est asynchrone (pareil avec
+    // setState), on a accès à la nouvelle valeur seulement au moment du rendu
+    // suivant du composant
+    // console.log(loading);
+
+    // console.log('on va charger les articles');
+
+    axios.get('https://oclock-open-apis.now.sh/api/blog/categories')
+      .then((response) => {
+        // callback exécutée en cas de succès (par exemple code de retour 200)
+        // console.log('success: ', response.data);
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        // callback exécutée en cas d'échec (par exemple code de retour 404)
+        console.log('error: ', error);
+      })
+      .finally(() => {
+        // callback exécutée dans tous les cas, après succès ou après échec =>
+        // permet notamment d'enlever un loader
+        // console.log('finally');
+        setLoadingCategories(false);
       });
     // console.log('on a lancé le chargement des articles');
   };
@@ -81,19 +109,23 @@ const Blog = () => {
   // }, [loading]);
 
   // si argument vide ([]) exécute seulement aprs le 1er rendu (componentDidMount)
-  // chargemùent des articles pres le 1er rendu
+  // chargement des articles et des catégories après le 1er rendu.
   useEffect(() => {
     // console.log('loading a changé de valeur');
     loadPosts();
+    loadCategories();
   }, []);
 
-  console.log('render blog component');
+  // console.log('render blog component');
+
+  const displayLoader = loadingCategories || loading;
 
   return (
     <div className="blog">
-      <Header categories={categories} />
-      { loading && (
+      {!loadingCategories && <Header categories={categories} />}
+      {displayLoader && (
         <Loader
+          className="loader"
           type="Bars"
           color="#9FEF02"
           height={50}
@@ -101,17 +133,19 @@ const Blog = () => {
           timeout={3000}
         />
       )}
-      <Switch>
-        <Redirect from="/jquery" to="/autre" />
-        {categories.map((category) => (
-          <Route exact path={category.route} key={category.label}>
-            <Posts posts={getPostByCategory(posts, category.label)} />
+      {!loadingCategories && (
+        <Switch>
+          <Redirect from="/jquery" to="/autre" />
+          {categories.map((category) => (
+            <Route exact path={category.route} key={category.label}>
+              <Posts posts={getPostByCategory(posts, category.label)} />
+            </Route>
+          ))}
+          <Route>
+            <Error404 />
           </Route>
-        ))}
-        <Route>
-          <Error404 />
-        </Route>
-      </Switch>
+        </Switch>
+      )}
       <Footer />
     </div>
   );
